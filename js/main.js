49e874c83c4cb171454b4d7e1134092c2b8295d6
Pavel-Surkov <surkovv.pavel@gmail.com>
2023-08-23 21:54:40 +0700

    feat: add desktop hero section

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//Hero swiper
+const heroSwiper = new Swiper('.hero-swiper', {
+  loop: true,
+  autoplay: true,
+  speed: 1500,
+  pagination: {
+    el: '.hero-swiper .swiper-pagination',
+  },
+});
